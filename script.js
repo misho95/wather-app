7dfc584f50da22f2515c73ecf0f47edccbe007fb
misho95 <chapidze.misho95@gmail.com>
2023-05-04 20:05:57 +0400

    ss

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
 
-const getLocation = async (city) => {
-    const cityName = city;
-    const APIkey = `b48f8d17db2c7e8becb6cc27a2e1c362`;
-    const respons = await fetch `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${APIkey}`;
-    const result = await respons.json();
-    console.log(result);
-}
+const addNewWeather = (temp, feelsLike, humi, windSpeed, visi, wether, city) => {
 
-// getLocation(`London`);
+    const d = new Date();
 
+    const day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ];
 
+    const fullDate = `${day[d.getDay()]} ,  ${d.getFullYear()} / ${d.getMonth()} / ${d.getDay()}`;
 
-$(`.add-weather`).click( () => {
-    $(`.modal-box`).show();
-});
-
-$(`#close-btn`).click( () => {
-    $(`.modal-box`).hide();
-});
-
-$(`#add-w-btn`).click( (e) => {
-    e.preventDefault()
-    let value = $(`.select-option`).val();
-        addNewWeather(value);
-    $(`.modal-box`).hide();
-});
-
-const addNewWeather = (city) => {
+    const icon = `<img src="./icons/sun.png" />`;
 
     const div = `
     <div class="weather-box">
@@ -37,25 +18,65 @@ const addNewWeather = (city) => {
     </button>
     <header class="weather-header">
         <div class="wh-left">
-            <img src="./icons/sun.png" />
+            ${icon}
         </div>
         <div class="wh-right">
             <div>${city}</div>
-            <div>Monday 01/17/2023</div>
+            <div>${fullDate}</div>
         </div>
     </header>
     <section class="weather-section">
-        <h1>15<sup>C</sup></h1>
-        <p>Mostly cloudy</p>
+        <h1>${temp}<sup>C</sup></h1>
+        <p>Mostly ${wether}</p>
     </section>
     <footer class="weather-footer">
-        <div>visibility 10km</div>
-        <div>feels like 10km</div>
-        <div>humidity 10km</div>
-        <div>wind 10km</div>
+        <div>visibility ${visi}</div>
+        <div>feels like ${feelsLike}</div>
+        <div>humidity ${humi}</div>
+        <div>wind ${windSpeed}</div>
     </footer>
 </div>
     `;
 
     $(`.add-weather`).before(div);
+};
+
+
+const getWeather = async (lon, lat, city) => {
+    const APIkey = `b48f8d17db2c7e8becb6cc27a2e1c362`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+    const respons = await fetch (url);
+    const result = await respons.json();
+    const feelsLike = result.main.feels_like;
+    const temp = result.main.temp;
+    const humi = result.main.humidity;
+    const windSpeed = result.wind.speed;
+    const visi = result.visibility;
+    const wether = result.weather[0].main;
+    addNewWeather( temp, feelsLike, humi, windSpeed, visi, wether,city);
+};
+
+const getLocation = async (city) => {
+    const APIkey = `b48f8d17db2c7e8becb6cc27a2e1c362`;
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIkey}`;
+    const respons = await fetch (url);
+    const result = await respons.json();
+    const lon = result[0].lon;
+    const lat = result[0].lat;
+    getWeather(lon, lat, city);
 }
+
+$(`.add-weather`).click( () => {
+    $(`.modal-box`).show();
+});
+
+$(`#close-btn`).click( () => {
+    $(`.modal-box`).hide();
+});
+
+$(`#add-w-btn`).click( (e) => {
+    e.preventDefault();
+    let value = $(`.select-option`).val();
+    getLocation(value);
+    $(`.modal-box`).hide();
+});
